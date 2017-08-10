0744b8dd0e98c7451dc57b79602ea2562c6db430
Nikos Kastellanos <nkastellanos@gmail.com>
2017-08-10 09:41:37 +0300

    Update AssemblyInfo.Portable.cs

--- Physics2D/Properties/AssemblyInfo.PORTABLE.cs
+++ Physics2D/Properties/AssemblyInfo.PORTABLE.cs
@@ -4,13 +4,13 @@ using System.Runtime.CompilerServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("Aether.Animation")]
+[assembly: AssemblyTitle ("Aether.Physics2D")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("Aether.Animation")]
-[assembly: AssemblyCopyright ("Copyright ©  Kastellanos Nikolaos 2011-2016")]
-[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCompany ("tainicom LTD")]
+[assembly: AssemblyProduct ("Aether.Physics2D")]
+[assembly: AssemblyCopyright ("Copyright Ian Qvist © 2013, Copyright © Kastellanos Nikolaos 2015-2017")]
+[assembly: AssemblyTrademark ("Aether Engine (tm)")]
 [assembly: AssemblyCulture ("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
