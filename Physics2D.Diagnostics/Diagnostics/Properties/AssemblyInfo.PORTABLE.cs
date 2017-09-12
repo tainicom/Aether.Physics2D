ff5e5424b15ac11281bce7537a09144a8c8e5184
Nikos Kastellanos <nkastellanos@gmail.com>
2017-09-12 15:05:13 +0300

    Add Portable project for Diagnostics

---
+++ Physics2D.Diagnostics/Diagnostics/Properties/AssemblyInfo.PORTABLE.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aether.Physics2D.Diagnostics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("tainicom LTD")]
+[assembly: AssemblyProduct("Aether.Physics2D")]
+[assembly: AssemblyCopyright("Copyright Ian Qvist © 2013, Copyright © Kastellanos Nikolaos 2015-2017")]
+[assembly: AssemblyTrademark("Aether Engine (tm)")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion ("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
