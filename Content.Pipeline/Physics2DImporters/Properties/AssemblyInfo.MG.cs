aca98ee3c025e227076aa3007eb911f4388e8cf5
Nikos Kastellanos <nkastellanos@gmail.com>
2018-01-02 22:08:52 +0200

    Upgrade libraries to Version 1.3

--- Content.Pipeline/Physics2DImporters/Properties/AssemblyInfo.MG.cs
+++ Content.Pipeline/Physics2DImporters/Properties/AssemblyInfo.MG.cs
@@ -10,7 +10,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("tainicom LTD")]
 [assembly: AssemblyProduct("Aether.Physics2D")]
-[assembly: AssemblyCopyright("Copyright Ian Qvist © 2013, Copyright © Kastellanos Nikolaos 2015-2017")]
+[assembly: AssemblyCopyright("Copyright Ian Qvist © 2013, Copyright © Kastellanos Nikolaos 2015-2018")]
 [assembly: AssemblyTrademark("Aether Engine (tm)")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.0.*")]
-[assembly: AssemblyFileVersion("1.2.0.*")]
+[assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyFileVersion("1.3.*")]
--- README.md
+++ README.md
@@ -29,6 +29,6 @@ https://www.nuget.org/packages/Aether.Physics2D
 
 https://www.nuget.org/packages/Aether.Physics2D.Diagnostics
 
-https://github.com/tainicom/Aether.Physics2D/releases/tag/v1.1
+https://github.com/tainicom/Aether.Physics2D/releases/tag/v1.2
 
 ![LightAndShadowsDemo](Documentation//Images/LightAndShadowsDemo.png)
