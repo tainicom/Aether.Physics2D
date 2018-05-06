982e422ead68b72d6968b4232d7b4e740f21c96f
Nikos Kastellanos <nkastellanos@gmail.com>
2018-05-06 17:33:14 +0300

    fix NullReferenceException when SkipSanityChecks = true.
    Move SkipSanityChecks in Triangulate.ConvexPartition()

--- Physics2D/Settings.cs
+++ Physics2D/Settings.cs
@@ -48,13 +48,6 @@ namespace tainicom.Aether.Physics2D
         public const bool EnableDiagnostics = true;
 
         /// <summary>
-        /// Set this to true to skip sanity checks in the engine. This will speed up the
-        /// tools by removing the overhead of the checks, but you will need to handle checks
-        /// yourself where it is needed.
-        /// </summary>
-        public const bool SkipSanityChecks = false;
-
-        /// <summary>
         /// The number of velocity iterations used in the solver.
         /// </summary>
         public static int VelocityIterations = 8;
