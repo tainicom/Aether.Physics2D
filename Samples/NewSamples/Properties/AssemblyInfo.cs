3db057a73e36a9ee35531486fad1568777d5a158
Nikos Kastellanos <nkastellanos@gmail.com>
2018-05-16 12:43:31 +0300

    Upgrade libraries to Version 1.3.1

--- Samples/NewSamples/Properties/AssemblyInfo.cs
+++ Samples/NewSamples/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.3.*")]
-[assembly: AssemblyFileVersion("1.3.*")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
 
 [assembly: NeutralResourcesLanguage("en")]
