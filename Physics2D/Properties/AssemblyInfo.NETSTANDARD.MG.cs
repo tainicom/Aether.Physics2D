f7de0702c2c77e9bf23147b8a0f4e5f47c8da27c
Nikos Kastellanos <nkastellanos@gmail.com>
2019-08-13 16:48:11 +0300

    Add NetStandard 2.0 projects

---
+++ Physics2D/Properties/AssemblyInfo.NETSTANDARD.MG.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aether.Physics2D")]
+[assembly: AssemblyDescription("Aether.Physics2D for MonoGame")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("tainicom LTD")]
+[assembly: AssemblyProduct("Aether.Physics2D")]
+[assembly: AssemblyCopyright("Copyright Ian Qvist © 2013, Copyright © Kastellanos Nikolaos 2015-2019")]
+[assembly: AssemblyTrademark("Aether Engine (tm)")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
