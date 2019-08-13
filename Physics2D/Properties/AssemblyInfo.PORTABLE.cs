890aed5157646aae272c47f6a13db5ea00796c57
Nikos Kastellanos <nkastellanos@gmail.com>
2019-08-13 18:32:48 +0300

    Normalize AssemblyInfo.PORTABLE.cs

--- Physics2D/Properties/AssemblyInfo.PORTABLE.cs
+++ Physics2D/Properties/AssemblyInfo.PORTABLE.cs
@@ -4,14 +4,14 @@ using System.Runtime.CompilerServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("Aether.Physics2D")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("tainicom LTD")]
-[assembly: AssemblyProduct ("Aether.Physics2D")]
-[assembly: AssemblyCopyright ("Copyright Ian Qvist © 2013, Copyright © Kastellanos Nikolaos 2015-2018")]
-[assembly: AssemblyTrademark ("Aether Engine (tm)")]
-[assembly: AssemblyCulture ("")]
+[assembly: AssemblyTitle("Aether.Physics2D")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("tainicom LTD")]
+[assembly: AssemblyProduct("Aether.Physics2D")]
+[assembly: AssemblyCopyright("Copyright Ian Qvist © 2013, Copyright © Kastellanos Nikolaos 2015-2018")]
+[assembly: AssemblyTrademark("Aether Engine (tm)")]
+[assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
