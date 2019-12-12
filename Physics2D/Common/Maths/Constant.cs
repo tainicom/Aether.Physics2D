df290967459bbf1cca06a951c23e85242d182695
Nikos Kastellanos <nkastellanos@gmail.com>
2019-12-12 10:16:07 +0200

    Add Constant class

---
+++ Physics2D/Common/Maths/Constant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tainicom.Aether.Physics2D.Common.Maths
+{
+    static class Constant
+    {
+        public const float Pi = (float)Math.PI;
+        public const float Tau = (float)(Math.PI * 2.0);
+    }
+}
