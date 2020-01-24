9fec7fe940e84f792f7cd84f2d5eae5bee22a97f
Nikos Kastellanos <nkastellanos@gmail.com>
2020-01-24 11:50:29 +0200

    Contidional compilation for Obsolete methods

--- Physics2D/Dynamics/World.cs
+++ Physics2D/Dynamics/World.cs
@@ -180,6 +180,7 @@ namespace tainicom.Aether.Physics2D.Dynamics
             Gravity = gravity;
         }
 
+#if XNAAPI
         /// <summary>
         /// Initializes a new instance of the <see cref="World"/> class.
         /// </summary>
@@ -188,6 +189,7 @@ namespace tainicom.Aether.Physics2D.Dynamics
         public World(AABB span) : this(new QuadTreeBroadPhase(span))
         {
         }
+#endif
 
         /// <summary>
         /// Initializes a new instance of the <see cref="World"/> class.
@@ -884,12 +886,14 @@ namespace tainicom.Aether.Physics2D.Dynamics
         /// </summary>        
         public bool IsLocked { get; private set; }
 
+#if XNAAPI
         /// <summary>
         /// Is the world running (in the middle of a time step).
         /// </summary>        
         /// <remarks>Deprecated in version 1.3</remarks>
         [Obsolete("Use IsLocked")]
         public bool IsStepping { get { return IsLocked; } }
+#endif
 
         /// <summary>
         /// Get the contact manager for testing.
@@ -1545,6 +1549,7 @@ namespace tainicom.Aether.Physics2D.Dynamics
             _queryAABBCallback = null;
         }
 
+#if XNAAPI
         /// <summary>
         /// Query the world for all fixtures that potentially overlap the provided AABB.
         /// Use the overload with a callback for filtering and better performance.
@@ -1565,6 +1570,7 @@ namespace tainicom.Aether.Physics2D.Dynamics
 
             return affected;
         }
+#endif
 
         /// <summary>
         /// Ray-cast the world for all fixtures in the path of the ray. Your callback
@@ -1592,6 +1598,7 @@ namespace tainicom.Aether.Physics2D.Dynamics
             _rayCastCallback = null;
         }
 
+#if XNAAPI
         /// <summary>
         /// Ray-cast the world for all fixtures in the path of the ray.
         /// Use the overload with a callback for filtering and better performance.
@@ -1612,6 +1619,7 @@ namespace tainicom.Aether.Physics2D.Dynamics
 
             return affected;
         }
+#endif
 
         /// <summary>
         /// Warning: This method is locked during callbacks.
