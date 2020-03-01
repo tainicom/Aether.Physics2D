5291c50bbedcf7e672864fdd48a4987729e0399b
Nikos Kastellanos <nkastellanos@gmail.com>
2020-03-01 14:46:43 +0200

    Refactor buoyancyController

--- Physics2D/Controllers/BuoyancyController.cs
+++ Physics2D/Controllers/BuoyancyController.cs
@@ -128,8 +128,8 @@ namespace tainicom.Aether.Physics2D.Controllers
                 body.ApplyForce(buoyancyForce, massc);
 
                 //Linear drag
-                Vector2 dragForce = body.GetLinearVelocityFromWorldPoint(areac) - Velocity;
-                dragForce *= -LinearDragCoefficient * area;
+                Vector2 dragVelocity = body.GetLinearVelocityFromWorldPoint(areac) - Velocity;
+                Vector2 dragForce = dragVelocity * (-LinearDragCoefficient * area);
                 body.ApplyForce(dragForce, areac);
 
                 //Angular drag
