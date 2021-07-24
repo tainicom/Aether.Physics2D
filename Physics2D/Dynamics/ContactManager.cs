d03a68cbcf052b5ba10e82b22ea0211436872113
Nick Kastellanos <nkastellanos@gmail.com>
2021-07-24 14:00:19 +0300

    internal ContactManager.BroadPhase

--- Physics2D/Dynamics/ContactManager.cs
+++ Physics2D/Dynamics/ContactManager.cs
@@ -67,7 +67,7 @@ namespace tainicom.Aether.Physics2D.Dynamics
         /// </summary>
         public BeginContactDelegate BeginContact;
 
-        public IBroadPhase BroadPhase;
+        public readonly IBroadPhase BroadPhase;
 
         public readonly ContactListHead ContactList;
         public int ContactCount { get; private set; }
