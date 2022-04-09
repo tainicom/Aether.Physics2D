ff17e4a1627de3d190b01c813c056eec37773d06
Nikos Kastellanos <nkastellanos@gmail.com>
2022-04-09 23:38:37 +0300

    Update samples (#107)
    * update nuget packages
    
    * shared projects
    
    shared
    
    * HelloWorldComponent
    
    * clean up samples game code
    
    * move platform code

---
+++ Samples/HelloWorld.WindowsDX/Program.cs
@@ -0,0 +1,17 @@
+namespace tainicom.Aether.Physics2D.Samples
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+}
+
---
+++ Samples/Testbed.WindowsDX/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aether.Physics2D.Samples.Testbed")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("tainicom LTD")]
+[assembly: AssemblyProduct("Aether.Physics2D")]
+[assembly: AssemblyCopyright("Copyright Ian Qvist © 2013, Copyright © Kastellanos Nikolaos 2015-2021")]
+[assembly: AssemblyTrademark("Aether Engine (tm)")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("BB07F235-56D0-46BD-8713-56F99936CF5A")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.6.1.0")]
+[assembly: AssemblyFileVersion("1.6.1.0")]
